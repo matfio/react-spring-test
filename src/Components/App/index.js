5f1a87afb7c8ea9e8f6487a9682715a0afb5359e
matfio <matfio@clio.me>
2022-02-18 11:01:16 +0100

    Animate simple mounting

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import Item from "../Item";
+import Container from "../Container";
 import "./index.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [isMounted, setIsMounted] = useState(false);
+
+  const doMount = () => {
+    setIsMounted((mounted) => !mounted);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={doMount}>Mount</button>
+      <Container>
+        <Item isVisible={isMounted} />
+      </Container>
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Item/index.js
+++ src/Components/Item/index.js
@@ -1,5 +1,35 @@
+import { useTransition, animated } from "react-spring";
 import "./index.css";
 
-const Item = () => {
-  return <div className="item"></div>;
+const from = {
+  x: -100,
+  y: 800,
+  opacity: 0,
 };
+
+const enter = {
+  x: 0,
+  y: 0,
+  opacity: 1,
+};
+
+const leave = {
+  x: 100,
+  y: 800,
+  opacity: 0,
+};
+
+const Item = ({ isVisible }) => {
+  const transition = useTransition(isVisible, {
+    from,
+    enter,
+    leave,
+  });
+
+  return transition(
+    (style, item) =>
+      item && <animated.div className="item" style={style}></animated.div>
+  );
+};
+
+export default Item;
